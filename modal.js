f80e2a87c8f4d83ce736fbb32413e75b3b6ede27
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-06-24 17:27:15 -0400

    Agregar `modal.js` con función para generar modales de productos.

---
+++ modal.js
@@ -0,0 +1,20 @@
+export const modal = (producto) => {
+    return `
+    <div class="modal fade" id="exampleModal${producto.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-body">
+            <img src="${producto.image}" class="w-100 mb-2" alt="...">
+            <h4 class="mb-3 ">${producto.title}</h4>
+            <p>${producto.description}</p>
+            <h1>$ ${producto.price.toLocaleString('es-CL')}</h1>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-link">Agregar al carrito</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+}
