cf836fae43c708a41c3e6d6e55499f91198fd111
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-06-25 17:28:26 -0400

    Integrar formulario para agregar productos y actualizar `script.js` con lógica de manejo de eventos y renderizado dinámico.

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 import {card} from './card.js';
 import {modal} from "./modal.js";
+import {modalAgregarProducto} from "./modalAgregarProducto.js";
+
+const body = document.querySelector('body');
+body.innerHTML += modalAgregarProducto();
+
+const seccionProductos = body.querySelector('#seccionProductos');
+const productos = [];
+
 
 (async () => {
-    const mainElement = document.querySelector('#main');
-    const productos = [];
     const response = await fetch('productos.json')
     const data = await response.json()
 
     productos.push(...data);
     productos.forEach((producto) => {
-        mainElement.innerHTML += card(producto);
-        mainElement.innerHTML += modal(producto);
+        seccionProductos.innerHTML += card(producto);
+        seccionProductos.innerHTML += modal(producto);
     })
 })()
+
+document.getElementById('formularioAgregarProducto').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const producto = {
+        "id": productos.length + 1,
+        "title": this.titulo.value,
+        "price": parseInt(this.precio.value),
+        "description": this.descripcion.value,
+        "image": this.imagen.value
+    }
+
+    console.log(producto);
+
+});
