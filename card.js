a6e0f33e4a2daee41b3ff86e54e377276ca4c292
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-06-25 20:43:30 -0400

    Se cambia el origen de apertura del modal que muestra el detalle del producto

--- card.js
+++ card.js
@@ -1,11 +1,11 @@
 export const card = (producto) => {
     return `
     <div class="card" style="width: 18rem;">
-        <img src="${producto.image}" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal${producto.id}">
+        <img src="${producto.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h6 class="card-title">${producto.title}</h6>
             <h4>$ ${producto.price.toLocaleString('es-CL')}</h4>
-            <a href="#" >Agregar al carrito</a>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal${producto.id}">Detalles</a>
         </div>
     </div>
     `
